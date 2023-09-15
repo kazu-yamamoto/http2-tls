0d89122867b7bd316ee188c8f508bfe201a5b43c
Edsko de Vries <edsko@well-typed.com>
2023-09-15 07:41:36 +0200

    Use `doctest` style comments for default settings

--- Network/HTTP2/TLS/Client/Settings.hs
+++ Network/HTTP2/TLS/Client/Settings.hs
@@ -6,24 +6,30 @@ import Network.Socket
 -- Client settings type.
 data Settings = Settings
     { settingsKeyLogger :: String -> IO ()
-    -- ^ Key logger (defaults to none)
+    -- ^ Key logger
     --
     -- Applications may wish to set this depending on the SSLKEYLOGFILE environment variable.
+    --
+    -- >>> settingsKeyLogger defaultSettings
+    -- \_ -> return ()
 
     , settingsValidateCert :: Bool
     -- ^ Should we validate TLS certificates?
     --
-    -- Defaults to @False@
+    -- >>> settingsValidateCert defaultSettings
+    -- True
 
     , settingsCAStore :: CertificateStore
     -- ^ Certificate store used for validation
     --
-    -- Defaults to @mempty@
+    -- >>> settingsCAStore defaultSettings
+    -- mempty
 
     , settingsAddrInfoFlags :: [AddrInfoFlag]
     -- ^ Flags that control the querying behaviour of @getAddrInfo@.
     --
-    -- Defaults to @[AI_ADDRCONFIG, AI_NUMERICHOST, AI_NUMERICSERV]@
+    -- >>> settingsAddrInfoFlags defaultSettings
+    -- []
     }
 
 -- | Default settings.
--- Network/HTTP2/TLS/Server/Settings.hs
+++ Network/HTTP2/TLS/Server/Settings.hs
@@ -4,20 +4,43 @@ module Network.HTTP2.TLS.Server.Settings where
 data Settings = Settings
     { settingsTimeout :: Int
     -- ^ Timeout in seconds. (All)
+    --
+    -- >>> settingsTimeout defaultSettings
+    -- 30
+
     , settingsSendBufferSize :: Int
     -- ^ Send buffer size. (H2 and H2c)
+    --
+    -- >>> settingsSendBufferSize defaultSettings
+    -- 4096
+
     , settingsSlowlorisSize :: Int
     -- ^ If the size of receiving data is less than or equal,
     --   the timeout is not reset.
     --   (All)
+    --
+    -- >>> settingsSlowlorisSize defaultSettings
+    -- 50
+
     , settingsReadBufferSize :: Int
     -- ^ When the size of a read buffer is lower than this limit, the buffer is thrown awany (and is eventually freed). Then a new buffer is allocated. (All)
+    --
+    -- >>> settingsReadBufferSize defaultSettings
+    -- 16384
+
     , settingsReadBufferLowerLimit :: Int
     -- ^  The allocation size for a read buffer.  (All)
+    --
+    -- >>> settingsReadBufferLowerLimit defaultSettings
+    -- 2048
+
     , settingsKeyLogger :: String -> IO ()
     -- ^ Key logger (defaults to none)
     --
     -- Applications may wish to set this depending on the SSLKEYLOGFILE environment variable.
+    --
+    -- >>> settingsKeyLogger defaultSettings
+    -- \_ -> return ()
     }
 
 -- | Default settings.
